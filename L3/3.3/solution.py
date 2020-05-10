c90f93d5a0ed4c83fde5bad128e9f9dcbb715ddd
divyajainsurana <43678200+divyajainsurana@users.noreply.github.com>
2020-05-10 12:07:08 +0800

    Update solution.py

--- L3/3.3/solution.py
+++ L3/3.3/solution.py
@@ -8,3 +8,20 @@ def solution(l):
     for x, v in d.items():
         if v: ans += sum(len(d[y]) for y in v)
     return ans
+
+
+or
+def solution(l):
+
+    count = 0
+    size = len(l)
+    if size < 3: return 0
+
+    cache = [0] * size
+    for x in xrange(size):
+        for y in xrange(x + 1, size):
+            if l[y] % l[x] == 0:
+                cache[y] += 1
+                count += cache[x]
+
+    return count
